7744ca401ceed8dd820653e49ac9056096a873a9
CamillaSantana <rm99491@fiap.com.br>
2023-11-23 12:11:57 -0300

    Api noticias

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# healthsure
+
+Chatbot
+<!-- 
+     
+
+<div className={styles.abrir}>
+
+<button className={styles.escondido}>
+            <BiSolidCommentDetail className={`${styles.icone} ${styles.iconMsg}`}/>
+            <IoClose className={`${styles.icone} ${styles.iconFechar}`} />
+        </button>
+
+
+        <section className={styles.chatbot}>
+
+                <header>
+                    <h2>ChatBot</h2>
+                </header>
+                <ul className={styles.chatbox}>
+                    <li className={`${styles.chat} ${styles.msgPc}`}>
+                        <FaRobot className={styles.icon}></FaRobot>
+                        <p>Olá! Como posso te ajudar?</p>
+                    </li>
+
+                    <li className={`${styles.chat} ${styles.msgUser}`}>
+                        <p>Lorem ipsum dolor sit amet.</p>
+                    </li>
+                </ul>
+
+            <div className={styles.chatInput}>
+                <textarea id="textarea" placeholder="Escreva sua mensagem"></textarea>
+                <MdSend id="enviar" className={styles.send}></MdSend>
+            </div>
+
+
+        </section> </div>  -->
--- src/app/principal/page.jsx
+++ src/app/principal/page.jsx
@@ -1,6 +1,17 @@
 "use client"
 import Cabecalho from "@/components/Cabecalho/Cabecalho";
 import Image from "next/image"
+import styles from "../../styles/Principal.module.css"
+import Link from "next/link";
+import { FaRobot } from "react-icons/fa";
+import { MdSend } from "react-icons/md";
+import { BiSolidCommentDetail } from "react-icons/bi";
+import { IoClose } from "react-icons/io5";
+import js from "./chat.js"
+import ChatBot from "@/components/ChatBot/Chatbot";
+
+
+
 import { useEffect, useState } from "react"
 
 export default function Principal(){
@@ -10,7 +21,7 @@ export default function Principal(){
 
     useEffect(() => {
 
-        fetch("http://localhost:8080/noticiasapi/webapi/noticia")
+        fetch("http://localhost:8080/healthsureapi/webapi/noticias")
         .then((response)=> response.json())
         .then((response)=> setNoticia(response))
         .catch(error=> console.error(error));
@@ -21,34 +32,35 @@ export default function Principal(){
 console.log(noticia)
 
 
-
 return(
+    <main className={styles.home}>
+
+        <Cabecalho></Cabecalho>
+
+        <section className={styles.noticia}>
+
+            <div className={styles.card}> {noticia.map((noticia) =>(
+
+                <div className={styles.cardItem} key={noticia.id}>
+                    <Image src={noticia.dsImagem} width={380} alt="imagem" height={260}></Image>
+                    <div className={styles.textos}>
+                    <h3>{noticia.nomeTitulo}</h3>
+                    <h5>Publicação - {noticia.dataNoticia}</h5>
+                    <p>{noticia.nomeSubtitulo}</p>
+                    <Link className={styles.linkMateria} href={noticia.dsLink}>Leia mais</Link>
+                    </div>
+                </div>))}
+        
+            </div>
+
+        </section>
 
 
-/*{ <ul>
-
-{
-    noticia.map((noticia) =>(
-
-        <li key={noticia.id}>
-            <p>{noticia.nomeTitulo}</p>
-          
-
-
-        </li>
+        {/* <ChatBot></ChatBot> */}
 
 
 
-    ))
-}
-
-
-
-</ul> }*/
-
-
-
+    </main>
 )
-
 
 }
