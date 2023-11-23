c5aa48276f79074d3af101600d9e2cfdcf73e3d4
CamillaSantana <rm99491@fiap.com.br>
2023-11-23 17:36:11 -0300

    em dev

--- src/app/api/usuarios/route.jsx
+++ src/app/api/usuarios/route.jsx
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(request, {params}){
 
-    const response = await fetch("http://localhost:8080/healthsureapi/webapi/noticias")
+    const response = await fetch("http://localhost:8080/healthsureapi/webapi/usuarios")
     const usuarios = await response.json()
 
 
--- src/app/login/[cadastro]/page.jsx
+++ src/app/login/[cadastro]/page.jsx
@@ -43,3 +43,4 @@ export default function Cadastro() {
     </main>
   );
 }
+
